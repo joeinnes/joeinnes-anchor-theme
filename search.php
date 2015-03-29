2ed8cd75a3d3db70e4d4c94617e3bc37d61d7acd
Joe Innes <joe@joeinn.es>
2015-03-29 18:47:19 +0200

    Added header text on search page

--- search.php
+++ search.php
@@ -2,16 +2,28 @@
 
 <div class="row">
 	<?php if(article_custom_field('featured_image')): ?>
-		<div class="featured-img" style="background-image: url(<?php echo article_custom_field('featured-image', 'http://s13.postimg.org/w3p4tc5pz/article_img_default.jpg'); ?>);"></div>
+		<div class="featured-img" style="background-image: url(<?php echo article_custom_field('featured-image', 'http://s13.postimg.org/w3p4tc5pz/article_img_default.jpg'); ?>);">
+			<div class="featured-overlay">
+				<div class="container">
+					<h1 class="article-header-headline">Search results</h1>
+				</div>
+			</div>
+		</div>
 	<?php else: ?>
-		<div class="featured-img" style="background-image: url('https://ununsplash.imgix.net/photo-1423753623104-718aaace6772?dpr=2&fit=crop&fm=jpg&h=600&q=75&w=1050');"></div>
+		<div class="featured-img" style="background-image: url('https://ununsplash.imgix.net/photo-1423753623104-718aaace6772?dpr=2&fit=crop&fm=jpg&h=600&q=75&w=1050');">
+			<div class="featured-overlay">
+				<div class="container">
+					<h1 class="article-header-headline">Search results</h1>
+				</div>
+			</div>
+		</div>
 	<?php endif?>
 </div>
 <div class="container">
 
-		<?php if(has_search_results()): ?>
-			<ul class="items">
-				<?php $i = 0; while(search_results()): $i++; ?>
+	<?php if(has_search_results()): ?>
+		<ul class="items">
+			<?php $i = 0; while(search_results()): $i++; ?>
 				<li style="background: hsl(215,28%,<?php echo round((($i / posts_per_page()) * 20) + 20); ?>%);">
 					<article class="wrap">
 						<h2>
@@ -19,21 +31,21 @@
 						</h2>
 					</article>
 				</li>
-				<?php endwhile; ?>
-			</ul>
+			<?php endwhile; ?>
+		</ul>
 
 		<section class="pagination">
 			<?php if(has_pagination()) : ?>
-			    <nav class="pagination">
+				<nav class="pagination">
 					<?php echo search_prev(); ?>
 					<?php echo search_next(); ?>
-			    </nav>
+				</nav>
 			<?php endif; ?>
 		</section>
 
-		<?php else: ?>
-			<p>No results for &ldquo;<?php echo search_term(); ?>&rdquo;</p>
-		<?php endif; ?>
+	<?php else: ?>
+		<p>No results for &ldquo;<?php echo search_term(); ?>&rdquo;</p>
+	<?php endif; ?>
 
 </div>
 
